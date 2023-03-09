712bb17d5a0ad934792da86a2ac4e80ec144c0fd
RUSHWAYC <rushwayc@yahoo.com>
2023-03-09 13:31:05 +0100

    Reasons left side done.

---
+++ src/components/Reasons/Reasons.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Reasons.css";
+
+import image1 from "../../assets/image1.png";
+import image2 from "../../assets/image2.png";
+import image3 from "../../assets/image3.png";
+import image4 from "../../assets/image4.png";
+import nb from "../../assets/nb.png";
+import adidas from "../../assets/adidas.png";
+import nike from "../../assets/nike.png";
+import tick from "../../assets/tick.png";
+
+const Reasons = () => {
+  return (
+    <div className="reasons" id="reasons">
+      <div className="left-r">
+        <img src={image1} alt="weigths" />
+        <img src={image2} alt="weigths" />
+        <img src={image3} alt="weigths" />
+        <img src={image4} alt="weigths" />
+      </div>
+      <div className="right-r">Right Side</div>
+    </div>
+  );
+};
+
+export default Reasons;
